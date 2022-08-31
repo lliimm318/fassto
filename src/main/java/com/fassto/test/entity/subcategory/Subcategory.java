c37db2059313bfb4a273b1f946d080209612ad78
임서영 <sylim0318@naver.com>
2022-08-31 14:18:35 +0900

    [UPDATE] category update add

--- src/main/java/com/fassto/test/entity/subcategory/Subcategory.java
+++ src/main/java/com/fassto/test/entity/subcategory/Subcategory.java
@@ -16,11 +16,11 @@ import javax.persistence.*;
 public class Subcategory {
 
     @Id
+    @Column(name = "full_code")
+    private String fullCode;
+
     @Column(name = "code")
     private String code;
-
-    @Column(name = "number")
-    private Integer number;
 
     @Column(name = "name")
     private String name;
@@ -31,7 +31,18 @@ public class Subcategory {
     @Column(name = "is_used")
     private Boolean isUsed;
 
-    @Column(name = "category_code")
-    private String categoryCode;
+    @ManyToOne
+    @JoinColumn(name = "category_code")
+    private Category category;
+
+    public Subcategory updateSubcategory(String categoryCode, String code, String name, Integer sort, Boolean isUsed) {
+        fullCode = categoryCode+code;
+        this.code = code;
+        this.name = name;
+        this.sort = sort;
+        this.isUsed = isUsed;
+
+        return this;
+    }
 
 }
