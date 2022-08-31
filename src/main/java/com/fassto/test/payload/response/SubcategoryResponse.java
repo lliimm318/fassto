9dcc1bd1344d2834044f3727e7ff560ecea9d6cf
임서영 <sylim0318@naver.com>
2022-08-31 22:21:36 +0900

    [UPDATE] id add

--- src/main/java/com/fassto/test/payload/response/SubcategoryResponse.java
+++ src/main/java/com/fassto/test/payload/response/SubcategoryResponse.java
@@ -2,10 +2,14 @@ package com.fassto.test.payload.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class SubcategoryResponse {
+
+    private final Integer id;
 
     private final Integer number;
 
