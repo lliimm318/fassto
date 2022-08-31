ed1ca61c1176bf2aec3a30dc29b86d799236b48d
임서영 <sylim0318@naver.com>
2022-08-31 14:18:58 +0900

    [FIX] private -> pubic

--- src/main/java/com/fassto/test/controller/CategoryController.java
+++ src/main/java/com/fassto/test/controller/CategoryController.java
@@ -1,11 +1,11 @@
 package com.fassto.test.controller;
 
+import com.fassto.test.payload.request.CategoryRequest;
+import com.fassto.test.payload.request.CodeRequest;
 import com.fassto.test.payload.response.CategoryResponse;
 import com.fassto.test.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +17,19 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/list")
-    private List<CategoryResponse> categoryList() {
+    public List<CategoryResponse> categoryList() {
         return categoryService.getCategories();
     }
 
-    
+    @PostMapping
+    public void createCategory(@RequestBody CategoryRequest categoryRequest) {
+        categoryService.createCategory(categoryRequest);
+    }
+
+    @DeleteMapping("/{categoryCode}")
+    public void deleteCategory(@PathVariable String code) {
+        categoryService.deleteCategory(code);
+    }
+
 
 }
