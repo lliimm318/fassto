a206df6f30d34397dea2b7c319eaa72c644fdd2c
임서영 <sylim0318@naver.com>
2022-08-31 22:27:15 +0900

    [UPDATE] quary

--- src/main/java/com/fassto/test/entity/category/CategoryRepository.java
+++ src/main/java/com/fassto/test/entity/category/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.fassto.test.entity.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,9 +15,13 @@ public interface CategoryRepository extends JpaRepository<Category, String> {
 
     Optional<Category> findByCode(String code);
 
-    /*
-    @Query(value = "select c from category c where (c.code like %a%) or (c.name like %a%)")
-    List<Category> findByCodeOrName(String word);
-     */
+
+    @Query(value = "select c from category c where (c.code like %:word%) or (c.name like %:word%)")
+    List<Category> findByCodeOrName(@Param("word") String word);
+
+
+    List<Category> findByCodeContains(String code);
+
+    List<Category> findByNameContains(String name);
 
 }
