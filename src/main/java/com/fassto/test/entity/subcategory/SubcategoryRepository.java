943854b937273866dde9a27a0f5a645d8fbd02bd
임서영 <sylim0318@naver.com>
2022-08-31 14:20:47 +0900

    [UPDATE] orm add

--- src/main/java/com/fassto/test/entity/subcategory/SubcategoryRepository.java
+++ src/main/java/com/fassto/test/entity/subcategory/SubcategoryRepository.java
@@ -1,11 +1,19 @@
 package com.fassto.test.entity.subcategory;
 
+import com.fassto.test.entity.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SubcategoryRepository extends JpaRepository<Subcategory, String> {
+
+    List<Subcategory> findByCategoryOrderBySort(Category category);
+
+    List<Subcategory> findByCategoryOrderBySortDesc(Category category);
+
+    Optional<Subcategory> findByFullCode(String fullCode);
 
 }
