f4bfc141d5effd38c4c565d03006f4fd6ad625e8
임서영 <sylim0318@naver.com>
2022-08-31 22:22:01 +0900

    [FIX] number delete

--- src/main/java/com/fassto/test/payload/response/CategoryResponse.java
+++ src/main/java/com/fassto/test/payload/response/CategoryResponse.java
@@ -2,12 +2,12 @@ package com.fassto.test.payload.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class CategoryResponse {
-
-    private final Integer number;
 
     private final String code;
 
