99706cb2228825a45516e2a775a457846a182e11
임서영 <sylim0318@naver.com>
2022-09-01 17:26:39 +0900

    [FIX] category delete

--- src/main/java/com/fassto/test/entity/Category.java
+++ src/main/java/com/fassto/test/entity/Category.java
@@ -1,6 +1,5 @@
-package com.fassto.test.entity.subcategory;
+package com.fassto.test.entity;
 
-import com.fassto.test.entity.category.Category;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,33 +9,32 @@ import javax.persistence.*;
 
 @Getter
 @Builder
-@Entity(name = "subcategory")
+@Entity(name = "category")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Subcategory {
+public class Category {
 
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "code")
+    @Column(name = "code", nullable = false, length = 6)
     private String code;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "is_used", nullable = false)
+    private Boolean isUsed;
 
     @Column(name = "sort")
     private Integer sort;
 
-    @Column(name = "is_used")
-    private Boolean isUsed;
+    @Column(name = "parents_code")
+    private String parentsCode;
 
-    @ManyToOne
-    @JoinColumn(name = "category_code",  referencedColumnName = "code")
-    private Category category;
-
-    public Subcategory updateSubcategory(String code, String name, Integer sort, Boolean isUsed) {
+    public Category update(String code, String name, Integer sort, Boolean isUsed) {
         this.code = code;
         this.name = name;
         this.sort = sort;
